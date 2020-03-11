b7b0b0bd53abd5265ab4e6895ccf5d574d59e2f6
ChiccoMosheSerby <chiccomoshe@gmail.com>
2020-03-11 22:37:55 +0200

    vp

--- client/src/view/Branch/Branch.js
+++ client/src/view/Branch/Branch.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
+    import React, { Component } from 'react';
 
-import './Branch.css';
+    import './Branch.css';
 
-class Branch extends Component {
-    
-    render() {
-        return (
+    class Branch extends Component {
+        
+        render() {
+            return (
 
-            <div className='branch'>
-                <div> {this.props.branchCity}</div>
+                <div className='branch'>
+                    <div> {this.props.branchCity}</div>
 
-                <div>{this.props.branchStreet} {this.props.branchStreetNum}</div>
-                <div>{this.props.branchPhoneNum}</div>
-                <div>א-ה: {this.props.branchObj.closeHour}-{this.props.branchObj.openHour}</div>
-                {
-                    this.props.branchObj.openHourWeekend !== '' ?
+                    <div>{this.props.branchStreet} {this.props.branchStreetNum}</div>
+                    <div>{this.props.branchPhoneNum}</div>
+                    <div>א-ה: {this.props.branchObj.closeHour}-{this.props.branchObj.openHour}</div>
+                    {
+                        this.props.branchObj.openHourWeekend !== '' ?
 
-                        <div>ו-ש: {this.props.branchObj.closeHourWeekend}-{this.props.branchObj.openHourWeekend}</div>
-                        :
-                        <div>ו-ש :סגור</div>
-                }
-            </div>
-        )
+                            <div>ו-ש: {this.props.branchObj.closeHourWeekend}-{this.props.branchObj.openHourWeekend}</div>
+                            :
+                            <div>ו-ש :סגור</div>
+                    }
+                </div>
+            )
+        }
     }
-}
 
 
-export default Branch;
+    export default Branch;
